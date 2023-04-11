b3014c6a893d86c355532b5f879cceff1fddba43
Alan <alanmonteiro105@gmail.com>
2023-04-10 22:53:13 -0300

    final release

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,55 @@
 import { useState } from 'react';
-import './App.css';
+import CardThanks from './components/CardThanks';
+import CardForm from './components/CardForm';
+import CardCredit from './components/CardCredit';
+import './styles/App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [formData, setFormData] = useState({
+    name: null,
+    number: null,
+    mm: null,
+    yy: null,
+    cvc: null,
+  });
+  const [validate, setValidate] = useState(false);
+
+  const animateSlider = (validate) => {
+    const axis = window.matchMedia('(max-width: 750px)').matches ? 'Y' : 'X';
+    document.querySelector(
+      '.cardOverflow > div',
+    ).style.transform = `translate${axis}(50${axis === 'Y' ? 'vh' : 'vw'})`;
+
+    document.body.classList.add('body-slider');
+
+    setTimeout(() => {
+      setValidate(validate);
+      document.body.classList.remove('body-slider');
+      document.querySelector('.cardOverflow > div').style.transform =
+        'translate(0)';
+    }, 500);
+  };
 
   return (
-    <div className="App">
-      0000 0000 0000 0000 Jane Appleseed 00/00 000 Cardholder Name e.g. Jane
-      Appleseed Card Number e.g. 1234 5678 9123 0000 Exp. Date (MM/YY) MM YY CVC
-      e.g. 123 Confirm Thank you! We've added your card details Continue
-    </div>
+    <>
+      <CardCredit formData={formData} />
+      <main className="cardOverflow">
+        <div>
+          {validate ? (
+            <CardThanks
+              setFormData={setFormData}
+              animateSlider={animateSlider}
+            />
+          ) : (
+            <CardForm
+              setFormData={setFormData}
+              animateSlider={animateSlider}
+              formData={formData}
+            />
+          )}
+        </div>
+      </main>
+    </>
   );
 }
-
 export default App;
---
+++ src/components/CardThanks.jsx
@@ -0,0 +1,20 @@
+import '../styles/CardThanks.css';
+import iconComplete from '../assets/images/icon-complete.svg';
+
+export default function CardThanks({ setFormData, animateSlider }) {
+  const resetForm = () => {
+    setFormData({ name: null, number: null, mm: null, yy: null, cvc: null });
+    animateSlider(false);
+  };
+
+  return (
+    <div className="cardThanks">
+      <img src={iconComplete} alt="" />
+      <p>Thank you!</p>
+      <p>We&apos;ve added your card details</p>
+      <button className="btn-primary" onClick={resetForm}>
+        Continue
+      </button>
+    </div>
+  );
+}
